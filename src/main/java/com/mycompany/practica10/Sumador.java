9bbb223369efa20b1fb6f5d83a36e062a60493ef
guille <guillermo.andres.gue@WIRTZWI-EHESK8H.wirtz.local>
2022-03-17 18:15:33 +0100

    primera version

---
+++ src/main/java/com/mycompany/practica10/Sumador.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.practica10;
+
+/**
+ *
+ * @author guillermo.andres.gue
+ */
+public class Sumador {
+ public static float sumar (float a, float b ) {
+return a + b;
+}} 
